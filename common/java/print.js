0dbce25052ca72e3e0116916087cdad3a38f2071
Tim Norris <tibben@ocf.berkeley.edu>
2018-11-28 09:40:15 -0500

    Update print.js

--- common/java/print.js
+++ common/java/print.js
@@ -3,7 +3,7 @@ if (( /print-pdf/gi ).test( window.location.search )) {
     var link  = document.createElement('link');
     link.rel  = 'stylesheet';
     link.type = 'text/css';
-    link.href = '../../common/css/print/pdf.css';
+    link.href = 'common/css/print/pdf.css';
     link.media = 'all';
     head.appendChild(link);
 }
